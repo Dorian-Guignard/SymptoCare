49d9b13ca0fe59668aa5915fa27e96ca024539c2
Dorian-Guignard <web.guidor@gmail.com>
2023-11-03 17:00:45 +0100

    tabComplete

--- src/components/Dashboard/ConstantTab/ConstantTab.js
+++ src/components/Dashboard/ConstantTab/ConstantTab.js
@@ -3,13 +3,119 @@ import React from "react";
 import "./ConstantTab.css";
 
 function ConstantTab({ tabClass }) {
+ let douleurSection = (
+   <section>
+     <div className="title-tab">
+       <h1>Douleurs</h1>
+     </div>
+     <div className='flex-logo-text'>
+     <div className="logo-tab">
+       {" "}
+       <img
+         src="/icons/IconTab/poignet.png"
+         alt="Image"
+         className="logo-tab douleurs"
+       />
+     </div>
+     <div className="text-tab">
+       <p>EN = 9</p>
+       <p className='text-date-time'>31/12/2023</p>
+     </div>
+     </div>
+   </section>
+ );
+ let cardioSection = (
+   <section>
+     <div className="title-tab">
+       <h1>Cardio</h1>
+     </div>
+     <div className="flex-logo-text">
+       <div className="logo-tab">
+         {" "}
+         <img
+           src="/icons/IconTab/sante.png"
+           alt="ImageCoeur"
+           className="logo-tab coeur"
+         />
+       </div>
+       <div className="text-tab coeur">        
+         <p>TA = 125/75</p>
+         <p>FC = 76</p>
+         <p className='text-date-time'>31/12/2023</p>
+       </div>
+     </div>
+   </section>
+ );
+  let temperatureSection = (
+   <section>
+     <div className="title-tab">
+       <h1>Température</h1>
+     </div>
+     <div className="flex-logo-text">
+       <div className="logo-tab">
+         {" "}
+         <img
+           src="/icons/IconTab/thermometre.png"
+           alt="ImageT°"
+           className="logo-tab temperature"
+         />
+       </div>
+       <div className="text-tab">        
+         <p>37,5°</p>
+         <p className='text-date-time'>31/12/2023</p>
+       </div>
+     </div>
+   </section>);
+     let poidsSection = (
+       <section>
+         <div className="title-tab">
+           <h1>Poids</h1>
+         </div>
+         <div className="flex-logo-text">
+           <div className="logo-tab">
+             {" "}
+             <img
+               src="/icons/IconTab/balance.png"
+               alt="Image de Balance"
+               className="logo-tab poids"
+             />
+           </div>
+           <div className="text-tab">             
+             <p>67,5 kg</p>
+             <p className='text-date-time'>31/12/2023</p>
+           </div>
+         </div>
+       </section>
+     );
+          let saturationSection = (
+            <section>
+              <div className="title-tab">
+                <h1>Saturation</h1>
+              </div>
+              <div className="flex-logo-text">
+                <div className="logo-tab">
+                  {" "}
+                  <img
+                    src="/icons/IconTab/oxymetre.png"
+                    alt="Image de saturometre"
+                    className="logo-tab saturation"
+                  />
+                  
+                </div>
+                <div className="text-tab">
+                  <p>99 %</p>
+                  <p className='text-date-time'>31/12/2023</p>
+                </div>
+              </div>
+            </section>
+          );
   return (
     <div className={`constantTab ${tabClass}`}>
-      {tabClass === "firstTab" ? "Douleurs" : ""}
-      {tabClass === "secondTab" ? "TA et FC" : ""}
-      {tabClass === "thirdTab" ? "Température" : ""}
-      {tabClass === "fourthTab" ? "Poids" : ""}
-      {tabClass === "fifthTab" ? "Saturation" : ""}
+      {tabClass === "firstTab" ? douleurSection : ""}
+      {tabClass === "secondTab" ? cardioSection : ""}
+      {tabClass === "thirdTab" ? temperatureSection : ""}
+      {tabClass === "fourthTab" ? poidsSection : ""}
+      {tabClass === "fifthTab" ? saturationSection : ""}
     </div>
   );
 }

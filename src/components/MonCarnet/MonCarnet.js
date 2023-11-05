30248dfe954f6dfd40c47db86f0de0036fa0ad62
Dorian-Guignard <web.guidor@gmail.com>
2023-11-05 12:51:07 +0100

    formMonCarnet

--- src/components/MonCarnet/MonCarnet.js
+++ src/components/MonCarnet/MonCarnet.js
@@ -1,14 +1,35 @@
 // Mon fichier MonCarnet.js
 import MenuNavigation from '../MenuNavigation/MenuNavigation';
 import "./MonCarnet.css";
-import { useLocation } from "react-router-dom";
+
 
 function MonCarnet() {
+let formCarnet = (
+    <div className="form-container">
+      <form className="form">
+        <div class="form-group">
+          <label for="email">Company Email</label>
+          <input required="" name="email" id="email" type="text" />
+    </div>
+        <div className="form-group">
+          <label for="textarea">How Can We Help You?</label>
+          <textarea required="" cols="50" rows="10" id="textarea" name="textarea">          </textarea>
+        </div>
+        <button type="submit" className="form-submit-btn">Submit</button>
+      </form>
+    </div>);
+
+let healthInformation = (
+  <div class="card">
+    <div class="card__content"></div>
+  </div>
+);
 
   return (
-    <div>
+    <div className="container-mon-carnet">
       <MenuNavigation />
-
+      { formCarnet }
+      { healthInformation }
     </div>
   );
 }
